a7729b6fee4525547d13453f6f9ec0f2d12e569a
axelder <38287158+Laserjonge@users.noreply.github.com>
2023-04-21 12:41:33 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,30 @@
 // JavaScript Document
 console.log("Howdy!");
+
+const boxes = Array.from(document.getElementsByClassName("ball"));
+
+let w = window.innerWidth;
+let h = window.innerHeight;
+console.log("Screen width: " + w);
+console.log("Screen height: " + h);
+
+let el = document.querySelector(".ball");
+let elCoordinates = el.getBoundingClientRect();
+
+
+
+
+boxes.forEach((box) => {
+	console.log("x " + elCoordinates.x);
+	console.log("y " + elCoordinates.y);
+
+
+  box.addEventListener("click", function (e) {
+    box.classList.toggle("balls-zoom-in");
+  });
+});
+
+
+
+
+
