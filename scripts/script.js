4ff4cb3c01ed87180dc60e279b926ffcab1ab162
axelder <38287158+Laserjonge@users.noreply.github.com>
2023-06-26 23:49:59 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -4,6 +4,9 @@ console.log("Howdy!");
 const balls = document.querySelectorAll("ol li div button");
 const information = document.querySelectorAll("ol li section");
 
+let outerCursor = document.querySelector(".outer-cursor");
+
+
 
 balls.forEach((ball) => {
   ball.addEventListener("click", function (e) {
@@ -12,11 +15,33 @@ balls.forEach((ball) => {
       info.classList.toggle("information-visible");
     })
   });
+
+  ball.addEventListener("mouseover", () => {
+    document.getElementById("cursor").src = "images/cursor-pointer.png";
+    })
+  ball.addEventListener("mouseleave", () => {
+    document.getElementById("cursor").src = "images/cursor-default.png";
+  })
 });
 
 
-// information.forEach((info) => {
-//   balls.addEventListener("click", function (e) {
-//     info.classList.toggle(".information-visible");
-//   });
-// });
+
+document.addEventListener("mousemove", moveCursor);
+
+function moveCursor(e){
+  let x = e.clientX;
+  let y = e.clientY;
+
+  outerCursor.style.left = `${x}px`;
+  outerCursor.style.top = `${y}px`;
+}
+// https://www.youtube.com/watch?v=Irwf2_1p7tQ
+
+
+const cursor =  document.getElementById('cursor');
+
+function opacity (){
+  cursor.classList.remove('cursor');
+}
+
+document.addEventListener("mousemove", opacity);
